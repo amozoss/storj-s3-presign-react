8d75dbdaa51b062ddd909209a07ed6b3c8be7377
Dan Willoughby <amozoss@gmail.com>
2022-09-29 13:37:27 -0600

    Add UI to upload to Storj using presign urls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(null);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+    try {
+      let file = event.target[0].files[0];
+      const url = `http://localhost:3001/presign`;
+      const params = new URLSearchParams({
+        key: file.name,
+      });
+      let resp = await fetch(url, {
+        method: "POST",
+        body: params,
+      });
+      let presignResp = await resp.json();
+
+      console.log("presign resp", presignResp);
+
+      resp = await fetch(presignResp.url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": file.type,
+        },
+        body: file,
+      });
+      console.log("resp", resp);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={onSubmit}>
+          <input type="file" />
+          <br />
+          <button disabled={loading}>Upload!</button>
+        </form>
       </header>
     </div>
   );
